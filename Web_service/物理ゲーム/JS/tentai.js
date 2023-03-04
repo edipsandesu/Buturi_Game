472db1bcb53b29f3db32240ea3a2dd3dbb282f84
sato <edipsandesu@gmail.com>
2023-03-05 02:00:46 +0900

    物理ゲーム

--- Web_service/物理ゲーム/JS/tentai.js
+++ Web_service/物理ゲーム/JS/tentai.js
@@ -5,32 +5,19 @@
         let start = Date.now(); //アニメーションの動く時間
         let mono_start = start; //物体の運動のための時間
 
-        //動く惑星の座標
-        let y = 500;
-        let x = 850;
-        let vx = 4;
-        let vy = 6;
-
-        //重力場1の座標
-        let M = -20000; //質量。-が引力、+が斥力になる。
-        let My = 500;
-        let Mx = 700;
-
-        //重力場2の座標
-        let M2 =  -45000; //質量
-        let M2y = 500;
-        let M2x = 1200;
-
-        let r = Math.sqrt((x-Mx)*(x-Mx) + (y-My)*(y-My));  //点電荷と重力場1の距離
-        let r2 = Math.sqrt((x-M2x)*(x-M2x) + (y-M2y)*(y-M2y)); //点電荷と重力場2の距離
+        //それぞれの天体の質量、座標、速度ベクトル。質量は-が引力
+        let MM = [-100, -120000, -500, -200];
+        let X = [600, 900, 1400, 400];
+        let Y = [1200, 800, 200, 600];
+        let Vx = [-3, 0, 6, 6];
+        let Vy = [-14, 0, 10, -10];
 
         //setIntervalは20msごとに実行される。第2引数より。
         let timer = setInterval(() => {
             let keika = Date.now() - mono_start;
             let anime_time = Date.now() - start;
 
-            console.log(r2);
-            if(anime_time >= 20000 || r2 < 40){
+            if(anime_time >= 30000){
                 clearInterval(timer);
             }
 
@@ -41,43 +28,34 @@
         function draw(time){
             
             let t = time / 8000;
+            let c = 10;  //rが極端に小さくなったときに速度が発散してしまうためこれを付け加えて一定の値を保つ
 
-            r = Math.sqrt((x-Mx)*(x-Mx) + (y-My)*(y-My));   //彗星と重力場1の距離の計算
-            r2 = Math.sqrt((x-M2x)*(x-M2x) + (y-M2y)*(y-M2y)); //彗星と重力場2の距離の計算
+            //天体の軌道計算
+            for(let i=0; i < MM.length; i++){
+                for(let j = 0; j < MM.length; j++){
+                    if(i != j){
+                        //対象の星とそれ以外のすべての距離を計算
+                        let r = Math.sqrt((X[i] - X[j])*(X[i] - X[j]) + (Y[i] - Y[j])*(Y[i] - Y[j]));
+
+                        if(r < 1){
+                            c = 10;
+                        }
+                        //対象の星とそれ以外の星から力を受けて変化する速度を加える
+                        Vx[i] += (X[i]- X[j])/(r*r*r + c)*MM[j]*t;
+                        Vy[i] += (Y[i]- Y[j])/(r*r*r + c)*MM[j]*t;
+                    }
+                }
+                X[i] += Vx[i]*t;
+                Y[i] += Vy[i]*t;
+            }
+
+            //天体の座標の更新
+            for(let i=0; i<MM.length; i++){
+                let mono = document.querySelectorAll("img")[i];
+                mono.style.top = Y[i] + 'px';
+                mono.style.left = X[i] + 'px';
+            }
             
-            /*
-            //彗星と重力場1の計算
-            vx += (rx-Mx)/(r*r*r) * M * t;
-            vy += (ry-My)/(r*r*r) * M * t;
-
-            rx += vx*t;
-            ry += vy*t;
-
-            //彗星と重力場2の計算
-            vx += (rx-M2x)/(r2*r2*r2) * M2 * t;
-            vy += (ry-M2y)/(r2*r2*r2) * M2 * t;
-
-            rx += vx*t;
-            ry += vy*t;
-            */
-
-            Tentai(t, Mx, My, M, r);
-            Tentai(t, M2x, M2y, M2, r2);
-
-            gazo.style.top = y + 'px';
-            gazo.style.left = x + 'px';
-        }
-        
-        function Tentai(t, mx, my, m, r){
-            //彗星と重力場1の計算
-            vx += (x-mx)/(r*r*r) * m * t;
-            vy += (y-my)/(r*r*r) * m * t;
-
-            x += vx*t;
-            y += vy*t;
-            
-            x += vx*t;
-            y += vy*t;
         }
     }
 
